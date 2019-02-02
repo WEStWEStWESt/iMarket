f752207b48d5fb71a900857a6a7a0654a5557305
D-CURT <https://github.com/D-CURT>
2019-02-02 20:23:53 +0300

    - BaseEntity added. - Lombok plugin installed.

---
+++ src/main/java/com/home_projects/imarket/models/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.home_projects.imarket.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Data
+@MappedSuperclass
+@NoArgsConstructor
+@AllArgsConstructor
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
