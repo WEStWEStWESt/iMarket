466f14114f2cbc04c69b215772b10cb0b9f7225b
D-CURT <https://github.com/D-CURT>
2019-02-02 18:05:32 +0300

    Roles and Profiles tables scripts added.

--- src/main/resources/sql/iMarketTables_v1.0.sql
+++ src/main/resources/sql/iMarketTables_v1.0.sql
@@ -1,9 +1,29 @@
 
 CREATE TABLE IF NOT EXISTS authorized_users (
-  id       BIGSERIAL      NOT NULL,
-  username VARCHAR(50)    NOT NULL    UNIQUE,
-  password VARCHAR(12)    NOT NULL    UNIQUE,
+  id              BIGSERIAL      NOT NULL,
+  username        VARCHAR(50)    NOT NULL    UNIQUE,
+  password        VARCHAR(12)    NOT NULL    UNIQUE,
 
   PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE IF NOT EXISTS roles (
+  id              BIGSERIAL      NOT NULL,
+  rolename        VARCHAR(50)    NOT NULL    UNIQUE,
+
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS profiles (
+  id              BIGSERIAL      NOT NULL,
+  name            VARCHAR(50)    NOT NULL    UNIQUE,
+  email           VARCHAR(50),
+  phone           VARCHAR(30),
+  address         VARCHAR(255),
+  passport        VARCHAR(20),
+
+  PRIMARY KEY (id)
+);
+
+
 
