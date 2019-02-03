d81d9cf9795751743e5cd1db999bbbd882bd331b
D-CURT <https://github.com/D-CURT>
2019-02-03 19:26:38 +0300

    AuthorizedUserRepository added.

---
+++ src/main/java/com/home_projects/imarket/dao/interfaces/AuthorizedUserRepository.java
@@ -0,0 +1,7 @@
+package com.home_projects.imarket.dao.interfaces;
+
+import com.home_projects.imarket.models.user.AuthorizedUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorizedUserRepository extends JpaRepository<AuthorizedUser, Long> {
+}
