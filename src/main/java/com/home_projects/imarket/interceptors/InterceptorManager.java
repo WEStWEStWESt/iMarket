28f2ec0afcd19f9b8f42ec715c1d8adf8b726453
D-CURT <https://github.com/D-CURT>
2019-02-09 18:28:20 +0300

    - InterceptorManager updated. - method execute() for interceptors added.

--- src/main/java/com/home_projects/imarket/interceptors/InterceptorManager.java
+++ src/main/java/com/home_projects/imarket/interceptors/InterceptorManager.java
@@ -1,36 +1,41 @@
 package com.home_projects.imarket.interceptors;
 
-import com.home_projects.imarket.interceptors.interfaces.Interceptor;
+import com.home_projects.imarket.interceptors.annotations.EntityInterceptor;
+import com.home_projects.imarket.interceptors.interfaces.*;
 import com.home_projects.imarket.models.BaseEntity;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class InterceptorManager {
     @Autowired
     private ApplicationContext context;
 
     public void onInit(BaseEntity entity) {
-        log.info(entity.getClass().getTypeName() + " initialization started...");
+        execute(entity, InitializationInterceptor.class);
     }
 
     public void onPrepare(BaseEntity entity) {
-        log.info(entity.getClass().getTypeName() + " preparing initiated...");
+        execute(entity, PreparingInterceptor.class);
     }
 
     public void onValidate(BaseEntity entity) {
-        log.info(entity.getClass().getTypeName() + " validation initiated...");
+        execute(entity, ValidationInterceptor.class);
     }
 
     public void onRemove(BaseEntity entity) {
-        log.info(entity.getClass().getTypeName() + " removing initiated...");
+        execute(entity, RemovingInterceptor.class);
     }
 
-    private void execute(BaseEntity entity, Class<Interceptor> type) {
-        context.getBeansOfType(type).values().stream().filter(interceptor -> );
+    private <I extends Interceptor> void execute(BaseEntity entity, Class<I> type) {
+        context.getBeansOfType(type)
+                .values()
+                .stream()
+                .filter(interceptor -> entity.getClass() == interceptor.getClass()
+                        .getAnnotation(EntityInterceptor.class)
+                        .interceptorEntity())
+                .forEach(interceptor -> interceptor.execute(entity));
     }
 
 
