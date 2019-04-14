78d399eda357cfaeb22192f15d4bc96b5a1fe0a1
D-CURT <https://github.com/D-CURT>
2019-04-14 19:03:09 +0300

    AuthorizationServerConfig added.

---
+++ src/main/java/com/home_projects/imarket/configurations/AuthorizationServerConfig.java
@@ -0,0 +1,90 @@
+package com.home_projects.imarket.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+@Configuration
+@EnableAuthorizationServer
+@PropertySource("classpath:/properties/credentials.properties")
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${security.oauth.client_id}")
+    private String clientId;
+
+    @Value("${security.oauth.client_secret}")
+    private String clientSecret;
+
+    @Value("${security.oauth.scope_read}")
+    private String read;
+
+    @Value("${security.oauth.scope_write}")
+    private String write;
+
+    @Value("${security.oauth.lifetime.access_token}")
+    private int lifetimeAccessToken;
+
+    @Value("${security.oauth.lifetime.refresh_token}")
+    private int lifetimeRefreshToken;
+
+    @Value("${security.oauth.grant_type.password}")
+    private String password;
+
+    @Value("${security.oauth.grant_type.refresh_token}")
+    private String refreshToken;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return new OAuth2AccessDeniedHandler();
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        security
+                .tokenKeyAccess("permitAll()")
+                .checkTokenAccess("isAuthenticated()")
+                .passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+                .inMemory()
+                .withClient(clientId)
+                .secret(clientSecret)
+                .scopes(read, write)
+                .accessTokenValiditySeconds(lifetimeAccessToken)
+                .refreshTokenValiditySeconds(lifetimeRefreshToken)
+                .authorizedGrantTypes(password, refreshToken);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+                .reuseRefreshTokens(false)
+                .authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+    }
+
+}
