e895d321b20478d9ac9c47316ad1cdbbed53d88c
D-CURT <https://github.com/D-CURT>
2019-04-14 19:14:01 +0300

    ResourceServerConfig added.

---
+++ src/main/java/com/home_projects/imarket/configurations/ResourceServerConfig.java
@@ -0,0 +1,21 @@
+package com.home_projects.imarket.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/oauth/**").permitAll()
+                .anyRequest().authenticated()
+                .and().logout().permitAll();
+    }
+
+}
